c137aabc172a354b0fdc6ac0e4a8f7182a3a9240
tsizion <tsionephrem7@gmail.com>
2024-07-03 16:01:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero title="new" />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default App;
